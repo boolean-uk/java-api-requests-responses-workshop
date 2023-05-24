95550393f5f47e2cc9c42a6424d03956586fa79a
Aidan van Geest <aidanvangeest@gmail.com>
2023-05-24 12:28:53 +0200

    Set up classes for exercise 2

---
+++ src/main/java/com/booleanuk/api/PublishersController.java
@@ -0,0 +1,11 @@
+package com.booleanuk.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/publishers")
+public class PublishersController {
+
+
+}
