771290e86cddd85e2592abc2d5c65b8a41cb809e
Dennis <dennisvoutos@gmail.com>
2023-05-24 13:36:23 +0300

    Exercise 2

---
+++ src/main/java/com/booleanuk/api/Publisher.java
@@ -0,0 +1,26 @@
+package com.booleanuk.api;
+
+public class Publisher {
+    private String name,city;
+
+    public Publisher(String name, String city) {
+        this.name = name;
+        this.city = city;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+}
