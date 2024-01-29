c4d602b1b061e893cbf44dfc258656ae79da4101
Robin Kaastrup <robin.kaastrup@no.experis.com>
2024-01-29 11:52:11 +0100

    Think this should be it for this morning exercise

---
+++ src/main/java/com/booleanuk/api/AuthorsController.java
@@ -0,0 +1,52 @@
+package com.booleanuk.api;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+
+@RestController
+@RequestMapping("authors")
+public class AuthorsController {
+    private ArrayList<Author> authors;
+
+    public AuthorsController() {
+        this.authors = new ArrayList<>();
+
+        this.authors.add(new Author("JRR Tolkien", "jrr@tolkien.com"));
+        this.authors.add(new Author("Jane Austen", "jane@austen.com"));
+    }
+
+    @GetMapping
+    public ArrayList<Author> getAll() {
+        return this.authors;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Author create(@RequestBody Author author) {
+        this.authors.add(author);
+        return author;
+    }
+
+    @GetMapping("/{id}")
+    public Author getSpecificAuthor(@PathVariable int id) {
+        if (this.authors.size() > id) {
+            return this.authors.get(id);
+        }
+
+        return null;
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Author update(@PathVariable int id, @RequestBody Author author) {
+        if (id < this.authors.size()) {
+            this.authors.get(id).setName(author.getName());
+            this.authors.get(id).setEmail(author.getEmail());
+            return this.authors.get(id);
+        }
+
+        return null;
+    }
+}
